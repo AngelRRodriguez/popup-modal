d8ff358fd1f1eb7aa423c4338f8799227619b55f
AngelRRodriguez <angelr500@hotmail.com>
2017-10-31 15:12:23 -0600

    Modal

---
+++ js/scripts.js
@@ -0,0 +1,200 @@
+
+$(document).ready(function() {
+	$("#start-modal").modal("show")
+	
+	setTimeout(function(){
+    $("#start-modal").modal("hide");
+     }, 3000);
+
+});
+
+
+var leon = document.getElementById('leon-audio');
+
+function playLeon() {
+	leon.play();
+
+    setTimeout(function(){
+        leon.pause();
+    }, 3000);
+}
+
+
+var serpeiente = document.getElementById('serpiente-audio');
+
+function playSerpiente() {
+	serpeiente.play();
+
+    setTimeout(function(){
+        serpeiente.pause();
+    }, 3000);
+}
+
+var zorro = document.getElementById('zorro-audio');
+
+function playZorro() {
+	zorro.play();
+
+    setTimeout(function(){
+        zorro.pause();
+    }, 4000);
+}
+
+var elefante = document.getElementById('elefante-audio');
+
+function playElefante() {
+	elefante.play();
+
+    setTimeout(function(){
+        elefante.pause();
+    }, 2000);
+}
+
+var aguila = document.getElementById('aguila-audio');
+
+function playAguila() {
+	aguila.play();
+
+    setTimeout(function(){
+        aguila.pause();
+    }, 3000);
+}
+
+var leopardo = document.getElementById('leopardo-audio');
+
+function playLeopardo() {
+	leopardo.play();
+
+    setTimeout(function(){
+        leopardo.pause();
+    }, 3000);
+}
+
+var conejo = document.getElementById('conejo-audio');
+
+function playConejo() {
+	conejo.play();
+
+    setTimeout(function(){
+        conejo.pause();
+    }, 3000);
+}
+
+var gato = document.getElementById('gato-audio');
+
+function playGato() {
+	gato.play();
+
+    setTimeout(function(){
+        gato.pause();
+    }, 5000);
+}
+
+var pajarito = document.getElementById('pajarito-audio');
+
+function playPajarito() {
+	pajarito.play();
+
+    setTimeout(function(){
+        pajarito.pause();
+    }, 3000);
+}
+
+var perro = document.getElementById('perro-audio');
+
+function playPerro() {
+	perro.play();
+
+    setTimeout(function(){
+        perro.pause();
+    }, 3000);
+}
+
+var pony = document.getElementById('pony-audio');
+
+function playPony() {
+	pony.play();
+
+    setTimeout(function(){
+        pony.pause();
+    }, 3000);
+}
+
+var cerdito = document.getElementById('cerdito-audio');
+
+function playCerdito() {
+	cerdito.play();
+
+    setTimeout(function(){
+        cerdito.pause();
+    }, 3000);
+}
+
+var pinguino = document.getElementById('pinguino-audio');
+
+function playPinguino() {
+	pinguino.play();
+
+    setTimeout(function(){
+        pinguino.pause();
+    }, 3000);
+}
+
+var ballena = document.getElementById('ballena-audio');
+
+function playBallena() {
+	ballena.play();
+
+    setTimeout(function(){
+        ballena.pause();
+    }, 5000);
+}
+
+var tortuga = document.getElementById('tortuga-audio');
+
+function playTortuga() {
+	tortuga.play();
+
+    setTimeout(function(){
+        tortuga.pause();
+    }, 3000);
+}
+
+var castor = document.getElementById('castor-audio');
+
+function playCastor() {
+	castor.play();
+
+    setTimeout(function(){
+        castor.pause();
+    }, 4000);
+}
+
+var foca = document.getElementById('foca-audio');
+
+function playFoca() {
+	foca.play();
+
+    setTimeout(function(){
+        foca.pause();
+    }, 6000);
+}
+
+var delfin = document.getElementById('delfin-audio');
+
+function playDelfin() {
+	delfin.play();
+
+    setTimeout(function(){
+        delfin.pause();
+    }, 5000);
+}
+
+
+
+
+
+
+
+
+
